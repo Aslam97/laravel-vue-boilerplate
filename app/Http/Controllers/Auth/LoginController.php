7a2cf919c9fb33c863ddfd89cc86dd9de60c2055
Aslam <iupin5212@gmail.com>
2020-11-10 01:20:48 +0700

    fix: loginController & add route validate

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class LoginController extends Controller
 {
@@ -57,7 +60,9 @@ class LoginController extends Controller
      */
     protected function authenticated(Request $request, $user)
     {
-        //
+        if ($request->wantsJson()) {
+            return new JsonResponse($user, Response::HTTP_OK);
+        }
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,13 @@
 
 use App\Http\Controllers\Api\LangController;
 use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\ValidateController;
 use Illuminate\Support\Facades\Route;
 
-Route::match(['put', 'patch'], 'locale', [LangController::class, 'update']);
+Route::group([], function ($route) {
+    Route::get('validate', [ValidateController::class, 'index']);
+    Route::match(['put', 'patch'], 'locale', [LangController::class, 'update']);
+});
 
 Route::middleware('auth:sanctum')->group(function ($route) {
     $route->get('/me', [UserController::class, 'me']);
