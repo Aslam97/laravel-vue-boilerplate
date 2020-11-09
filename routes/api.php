7dd51288c1ca47aefc30207981cdd19332e561e8
Aslam <iupin5212@gmail.com>
2020-11-09 16:23:10 +0700

    add locale route

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\Api\LangController;
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
+
+Route::match(['put', 'patch'], 'locale', [LangController::class, 'update']);
 
 Route::middleware('auth:sanctum')->group(function ($route) {
     $route->get('/user', [UserController::class, 'show']);
