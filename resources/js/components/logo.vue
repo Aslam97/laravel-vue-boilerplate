82a204ce4b582316a1af570f80c23623571cc17f
Aslam <iupin5212@gmail.com>
2020-11-14 21:33:28 +0700

    Update

--- resources/js/components/logo.vue
+++ resources/js/components/logo.vue
@@ -1,8 +1,8 @@
 <template functional>
   <img
-    v-lazy="require('@assets/images/logo.svg')"
+    :src="require('@assets/images/logo.svg')"
     width="200"
     height="70"
     alt="Logo"
-  >
+  />
 </template>
