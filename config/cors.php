12b8cb265987a2c687094c7dc6b43d5ae12aa5e1
Aslam <iupin5212@gmail.com>
2020-11-14 14:52:46 +0700

    Update config

--- config/cors.php
+++ config/cors.php
@@ -13,9 +13,14 @@ return [
     |
     | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
     |
-    */
+     */
 
-    'paths' => ['api/*'],
+    'paths' => [
+        'api/*',
+        '/login',
+        '/logout',
+        '/register',
+    ],
 
     'allowed_methods' => ['*'],
 
@@ -29,6 +34,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
